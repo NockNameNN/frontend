c909d16f650fe13d063cb9d913073999cd89253a
NockNameNN <nockname2@mail.ru>
2023-10-13 15:03:52 +0700

    1

---
+++ task-stage-2-main/26/main.js
@@ -0,0 +1,16 @@
+const number = new Number(500)
+console.dir(number)
+console.dir(Number.prototype.__proto__)
+console.dir(Number.prototype.__proto__.__proto__)
+
+const string = new String("q")
+console.dir(string)
+console.dir(String.prototype.__proto__)
+console.dir(String.prototype.__proto__.__proto__)
+
+const boolean = new Boolean(True)
+console.dir(boolean)
+console.dir(Boolean.prototype.__proto__)
+console.dir(Boolean.prototype.__proto__.__proto__)
+
+
---
+++ task-stage-2-main/29/name.js
@@ -0,0 +1,11 @@
+const firstName = "Ruslan";
+const lastName = "Sagdiev";
+const patronymic = "Ravilievich"
+const year_of_birth = 2003;
+const direction_at_institute = "PI"
+
+const result1 = ("firstName: " + firstName + "\nlastName: " + lastName + "\npatronymic: " + patronymic + "\nYear of birth: " + year_of_birth + "\nDirection at the institute: " + direction_at_institute);
+const result2 = (`firstName: ${firstName} \nlastName: ${lastName} \npatronymic: ${patronymic} \nYear of birth: ${year_of_birth} \nDirection at the institute: ${direction_at_institute}`);
+
+console.log(result1);
+console.log(result2);
