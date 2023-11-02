73e3f5a2e5346ff4a563eb5def65a04d9ee91b9a
NockNameNN <99240727+NockNameNN@users.noreply.github.com>
2023-11-03 00:41:27 +0700

    66 - dop

--- task-stage-3-main/66 - dop/index.js
+++ task-stage-3-main/66 - dop/index.js
@@ -1,6 +1,8 @@
-import { getProducts } from "./requests.js";
+import { getProducts, getCategories, fetchProductsByCategory, fetchSearchByProducts } from "./requests.js";
 
 const list = document.querySelector('.js-list')
+const select = document.querySelector('.js-select')
+const search = document.querySelector('.js-search')
 
 getProducts().then((data) => {
     data.products.forEach((item) => {
@@ -9,3 +11,35 @@ getProducts().then((data) => {
         list.append(li)
     })
 })
+
+getCategories().then(data => {
+    data.forEach(e => {
+        const option = document.createElement('option')
+        option.text = e
+        select.appendChild(option)
+    })
+})
+
+select.addEventListener('change', el => {
+    list.innerHTML = ''
+    fetchProductsByCategory(el.target.value).then(data => {
+        data.products.forEach(e => {
+            const li = document.createElement('li')
+            li.textContent = e.title
+            list.append(li)
+        })
+    })
+})
+
+search.addEventListener('change', el => {
+    list.innerHTML = ''
+    fetchSearchByProducts(el.target.value).then(data => {
+        data.products.forEach(e => {
+            const li = document.createElement('li')
+            li.textContent = e.title
+            list.append(li)
+        })
+    })
+})
+
+
--- task-stage-3-main/66 - dop/requests.js
+++ task-stage-3-main/66 - dop/requests.js
@@ -2,3 +2,18 @@ export function getProducts() {
     const url = 'https://dummyjson.com/products/';
     return fetch(url).then((response) => response.json());
 }
+
+export function getCategories() {
+    return fetch('https://dummyjson.com/products/categories').
+        then(response => response.json());
+}
+
+export function fetchProductsByCategory(category) {
+    return fetch(`https://dummyjson.com/products/category/${category}`)
+        .then(response => response.json())
+}
+
+export function fetchSearchByProducts(word) {
+    return fetch(`https://dummyjson.com/products/search?q=${word}`)
+        .then(response => response.json()) 
+}
