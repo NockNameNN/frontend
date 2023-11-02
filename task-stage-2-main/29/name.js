6b95da101669adedb71d30d0cbc40971f3979838
NockNameNN <99240727+NockNameNN@users.noreply.github.com>
2023-11-02 14:12:49 +0700

    Update name.js

--- task-stage-2-main/29/name.js
+++ task-stage-2-main/29/name.js
@@ -5,7 +5,11 @@ const year_of_birth = 2003;
 const direction_at_institute = "PI"
 
 const result1 = ("firstName: " + firstName + "\nlastName: " + lastName + "\npatronymic: " + patronymic + "\nYear of birth: " + year_of_birth + "\nDirection at the institute: " + direction_at_institute);
-const result2 = (`firstName: ${firstName} \nlastName: ${lastName} \npatronymic: ${patronymic} \nYear of birth: ${year_of_birth} \nDirection at the institute: ${direction_at_institute}`);
+const result2 = (`firstName: ${firstName} 
+lastName: ${lastName} 
+patronymic: ${patronymic} 
+Year of birth: ${year_of_birth} 
+Direction at the institute: ${direction_at_institute}`);
 
 console.log(result1);
 console.log(result2);
